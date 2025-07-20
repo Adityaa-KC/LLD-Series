5e2cc5cd1c7671d1dfb720417872ec1761755a1e
Adityaa-kc <adityakumarchaudhary1623@gmail.com>
2025-07-20 11:28:28 +0530

    Day 3 all concepts revised

--- Day3.cpp
+++ Day3.cpp
@@ -5,13 +5,17 @@ private:
     string model;
     string Engnum;
     string Brand;
-    int speed = 0;
 public:
+    int speed = 0;
+    Car(){
+       cout<<"Welcome in the Beast Car !!"<<endl<<endl;
+    }
     Car(string m,string Engno,string brand){
         this->model = m;
         this->Engnum = Engno;
         this->Brand = brand;
     }
+    void setInfo();
     void carDet();
     void startEng();
     void stopEng();
@@ -20,22 +24,42 @@ public:
 };
 
 class manualCar : public Car{
+    int fuel = 10;
 public:
+    manualCar(){
+        // setInfo();
+    }
     void change_gear();
     void fuel_indication();
 };
 
 class elecCar : public Car{
+    int bp = 100 ;
 public:
+    elecCar(){
+        // setInfo();
+    }
     void Battery_percentage();
     void is_batt_charging();
 };
+
+void Car :: setInfo(){
+    cout<<"Enter model type :";
+    cin>>model;
+    cout<<"Enter Engine number :";
+    cin>>Engnum;
+    cout<<"Enter Car brand :";
+    cin>>Brand;
+    cout<<endl<<endl;
+}
+
+
 
 void Car :: carDet(){
     cout<<"Details of the following car is : "<<endl;
     cout<<"Brand :"<<Brand<<endl;
     cout<<"Engine number :"<<Engnum<<endl;
-    cout<<"Model :"<<model<<endl;
+    cout<<"Model :"<<model<<" Transmission"<<endl;
 }
 
 void Car :: startEng(){
@@ -53,10 +77,12 @@ void Car :: stopEng(){
 }
 
 void Car :: brake(){
-    if (speed >= 0){
+    if (speed > 0){
         speed -= 10;
         cout<<"Brake applied succesfully !!"<<endl;
     }
+    else 
+    cout<<"Vehicle is standing "<<endl;
     cout<<"Current speed : "<<speed<<endl;
 }
 
@@ -72,7 +98,28 @@ void Car :: accelerate(){
 }
 
 void manualCar :: change_gear(){
-    cout<<"Gear changed";
+    cout<<"Gear changed"<<endl;
+    fuel -= 1;
+}
+
+void manualCar :: fuel_indication(){
+    if (fuel > 5)
+    cout<<"Sufficient fuel"<<endl;
+    else if (fuel>3)
+    cout<<"Medium fuel"<<endl;
+    else 
+    cout<<"Go to Petrol Pump URGENTLY "<<endl;
+}
+
+void elecCar :: Battery_percentage(){
+    cout<<"Battery Percentage : "<<bp<<"%"<<endl;
+}
+
+void elecCar :: is_batt_charging(){
+    if (speed == 0)
+    cout<<"Battery is charging "<<endl;
+    else 
+    cout<<"Battery is not charging "<<endl;
 }
 
 
@@ -84,4 +131,22 @@ int main(){
     car->accelerate();
     car->brake();
     car->stopEng();
+    
+    elecCar car2;
+    car2.carDet();
+    car2.startEng();
+    car2.accelerate();
+    car2.Battery_percentage();
+    car2.is_batt_charging();
+    car2.brake();
+    car2.stopEng();
+    
+    manualCar car3;
+    car3.carDet();
+    car3.startEng();
+    car3.accelerate();
+    car3.change_gear();
+    car3.fuel_indication();
+    car3.brake();
+    car3.stopEng();
 }
